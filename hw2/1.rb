7d6357f3ebf3e5dc9e3501db886e1b6a4b56cfd4
John <john@virt-arch-elio>
2012-03-16 17:28:43 +0300

    drafted hw2/1

--- hw2/1.rb
+++ hw2/1.rb
@@ -1,14 +1,53 @@
 # metaprogramming to the rescue!
 
 class Numeric
-  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
-  def method_missing(method_id)
-    singular_currency = method_id.to_s.gsub( /s$/, '')
+  @@currencies = {'dollar' => 1, 'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+
+  def __common_stuff (curr)
+    singular_currency = curr.to_s.sub( /s$/, '')
     if @@currencies.has_key?(singular_currency)
-      self * @@currencies[singular_currency]
+      yield @@currencies[singular_currency]
     else
-      super
+      yield
     end
   end
+
+  def method_missing(method_id)
+    __common_stuff(method_id) do |q| q ? self * q : super; end
+  end
+
+  def in (dest)
+    __common_stuff(dest) do |q| q ? self / q : self; end
+  end
 end
+
+class String
+  def palindrome?
+    self == self.reverse        # FIXME: insert hw1 solution
+  end
+end
+
+module Enumerable
+  def palindrome?
+    self == self.reverse
+  end
+end
+
+%Q{
+def test
+  yield 1
+end
+
+test { |x| yield x+1 }
+}
+
+puts (10 + 1).euros.in :yen
+
+puts [3,4,3,4].palindrome?
+puts 'abcba'.palindrome?
+
+
+
+
+
 
