6522733d0a83146d3534d274d0220de2e669cb4b
John <john@arch-virt-home>
2012-03-24 21:12:25 +0300

    added hw3 features

---
+++ hw3/features/step_definitions/movie_steps.rb
@@ -0,0 +1,74 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /the following movies exist/ do |movies_table|
+
+  count = 0
+
+  movies_table.hashes.each do |movie|
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+
+    count += 1
+
+    Movie.create!(movie)
+  end
+
+  puts "Added #{count} movies"
+
+
+
+  #assert false, "Unimplmemented"
+end
+
+Then /^I should see all of the movies/ do
+  Movie.select(:title).each do |t|
+    step %Q{I should see "#{t.title}"}
+  end
+end
+
+Then /^I should see none of the movies/ do
+  Movie.select(:title).each do |t|
+    step %Q{I should not see "#{t.title}"}
+  end
+end
+
+
+# Make sure that one string (regexp) occurs before or after another one
+#   on the same page
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.content  is the entire content of the page as a string.
+
+  #page.body =~ Regexp.new("#{e1}.+#{e2}")
+  #puts find("table#movies").text
+
+  count=0
+
+  Movie.all.each { count += 1 }
+
+  puts "Found #{count} movies"
+
+  puts page.body
+
+  assert false
+end
+
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
+
+When /I (un)?check the following ratings:\s*(.*)\s*$/ do |uncheck, rating_list|
+  # HINT: use String#split to split up the rating_list, then
+  #   iterate over the ratings and reuse the "When I check..." or
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(/\s*,\s*/).each do |r|
+    if r =~ /.+/ then
+      if uncheck then
+        step %Q{I uncheck "ratings_#{r}"}
+      else
+        step %Q{I check "ratings_#{r}"}
+      end
+    end
+  end
+end
